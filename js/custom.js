c51bb7a4da0740b01738828c94de089fc10bd50a
dino <dinojoaocosta@gmail.com>
2023-02-19 20:54:43 +0000

    Configure Google Analytics 4

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LF9R8Y4KG2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-LF9R8Y4KG2');
+</script>
