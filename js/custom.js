4dcd6211b0b675d666ef99d004fb04fd4d0a4f27
dino <dinojoaocosta@gmail.com>
2023-02-19 22:03:37 +0000

    Fix Google Analytics 4?

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-LF9R8Y4KG2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+// <!-- Google tag (gtag.js) -->
+var script = document.createElement("script");
+script.src = "https://www.googletagmanager.com/gtag/js?id=G-LF9R8Y4KG2";
+document.head.appendChild(script);
 
-  gtag('config', 'G-LF9R8Y4KG2');
-</script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-LF9R8Y4KG2');
